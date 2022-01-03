5827eddb4789f7d9136b34798762316db85890dc
fleroviux <fleroviux@gmail.com>
2022-01-03 21:36:34 +0100

    Common: add a basic ArrayView template

---
+++ src/common/include/aurora/array_view.hpp
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2022 fleroviux
+ */
+
+#pragma once
+
+#include <stddef.h>
+
+namespace Aura {
+
+template<typename T>
+struct ArrayView {
+  using iterator = T*;
+  using const_iterator = T const*;
+
+  constexpr ArrayView(T* data, size_t size) : data_(data), size_(size) {}
+
+  constexpr auto cbegin() const -> const_iterator {
+    return data();
+  }
+
+  constexpr auto begin() -> iterator {
+    return data();
+  }
+
+  constexpr auto cend() const -> const_iterator {
+    return data() + size();
+  }
+
+  constexpr auto end() -> iterator {
+    return data() + size();
+  }
+
+  constexpr auto data() const -> T const* {
+    return data_;
+  }
+
+  constexpr auto data() -> T* {
+    return data_;
+  }
+
+  constexpr auto size() const -> size_t {
+    return size_;
+  }
+
+  constexpr bool empty() const {
+    return size() != 0;
+  }
+
+  constexpr auto operator[](size_t i) const -> T const& {
+    return data()[i];
+  }
+
+  constexpr auto operator[](size_t i) -> T& {
+    return data()[i];
+  }
+
+private:
+  T* data_;
+  size_t size_;
+};
+
+}; // namespace Aura
