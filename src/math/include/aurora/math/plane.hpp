e0eb9957f28ffb432ed5e681c8b54a91f700546f
fleroviux <fleroviux@gmail.com>
2022-04-15 00:02:14 +0200

    Math: document the plane class

--- src/math/include/aurora/math/plane.hpp
+++ src/math/include/aurora/math/plane.hpp
@@ -10,14 +10,26 @@ namespace Aura {
 
 namespace detail {
 
-template<typename T, typename Vector3>
+/**
+ * A parametric plane defined by a normal vector and a signed distance from the origin along the normal.
+ *
+ * @tparam T the underlying data type (i.e. float)
+ * @tparam Vec3 the three-dimensional vector type to be used
+ */
+template<typename T, typename Vec3>
 struct Plane {
+  /**
+   * Default constructor. The plane is initialized to lie at (0, 0, 0) and point upwards (on the y-Axis).
+   */
   Plane() {}
 
-  Plane(Vector3 const& normal) : normal(normal), distance(NumericConstants<T>::zero()) {
-  }
-
-  Plane(Vector3 const& normal, T distance) : normal(normal), distance(distance) {
+  /**
+   * Construct a Plane from a normal vector and a signed distance.
+   *
+   * @param normal the normal vector
+   * @param distance the signed distance from the origin
+   */
+  Plane(Vec3 const& normal, T distance = NumericConstants<T>::zero()) : normal(normal), distance(distance) {
   }
 
   auto x() -> T& { return normal.x(); }
@@ -30,31 +42,56 @@ struct Plane {
   auto z() const -> T { return normal.z(); }
   auto w() const -> T { return normal.w(); }
 
-  auto get_normal() const -> Vector3 {
+  /**
+   * Get the normal vector of this plane.
+   * @return the normal vector
+   */
+  auto get_normal() const -> Vec3 {
     return normal;
   }
 
-  void set_normal(Vector3 const& normal) {
+  /**
+   * Set the normal vector for this plane.
+   * 
+   * @param normal the normal vector
+   */
+  void set_normal(Vec3 const& normal) {
     this->normal = normal;
   }
 
+  /**
+   * Get the signed distance of this plane from the origin.
+   */
   auto get_distance() const -> T {
     return distance;
   }
 
+  /**
+   * Set the signed distance from the origin for this plane.
+   */
   void set_distance(T distance) {
     this->distance = distance;
   }
 
-  auto get_distance_to_point(Vector3 const& point) const -> T {
+  /**
+   * Compute the signed distance of a point from this plane.
+   *
+   * @param point the point
+   * @return the signed distance
+   */
+  auto get_distance_to_point(Vec3 const& point) const -> T {
     return point.x() * normal.x() +
            point.y() * normal.y() +
            point.z() * normal.z() - distance;
   }
 
 private:
-  Vector3 normal;
-  T distance;
+  Vec3 normal{
+    NumericConstants<T>::zero(),
+    NumericConstants<T>::one(),
+    NumericConstants<T>::zero()
+  };
+  T distance = NumericConstants<T>::zero();
 };
 
 } // namespace Aura::detail
